0d305eca308a6beb458bf9b148cfeff95aefc050
huelton.santos <hueltondihon@gmail.com>
2018-01-25 18:51:17 -0200

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -27,10 +27,7 @@ export class CategoriasPage {
              response => {
                this.items = response;
              },
-             error => {
-               console.log(error);
-           }
-        );
+             error => {});
   }
 
 }
