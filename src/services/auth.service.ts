ac75000f4ca7122c2e3d91ebb9edffbb6fedd29b
huelton.santos <hueltondihon@gmail.com>
2018-01-26 21:21:12 -0200

    Criando uma pagina de Profile

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,9 @@
-import { StorageService } from './storage.service';
-import { LocalUser } from './../models/local_user';
-import { API_CONFIG } from './../config/api.config';
-import { HttpClient } from '@angular/common/http';
-import { CredenciaisDTO } from './../models/credenciais.dto';
 import { Injectable } from '@angular/core';
+import { CredenciaisDTO } from './../models/credenciais.dto';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from './../config/api.config';
+import { LocalUser } from './../models/local_user';
+import { StorageService } from './storage.service';
 import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
