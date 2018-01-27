53d12ff0e78c8f9d800cf5a4c7250aac5a08b420
huelton.santos <hueltondihon@gmail.com>
2018-01-27 12:58:22 -0200

    Tratamento padrão para outros erros com Alert

--- src/Interceptors/error-interceptor.ts
+++ src/Interceptors/error-interceptor.ts
@@ -2,6 +2,7 @@ import { StorageService } from './../services/storage.service';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from 'rxjs/Rx';
+import { AlertController } from "ionic-angular";
 
 
 
@@ -9,7 +10,9 @@ import { Observable } from 'rxjs/Rx';
 export class ErrorInterceptor implements HttpInterceptor {
 
 
-    constructor(public storage: StorageService){        
+    constructor(
+        public storage: StorageService,
+        public alertCtrl: AlertController){        
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -28,17 +31,74 @@ export class ErrorInterceptor implements HttpInterceptor {
              console.log(errorObj);
 
              switch(errorObj.status) {
-                 case 403: 
+                case 401: 
+                 this.handle401();
+                 break; 
+                
+                case 403: 
                  this.handle403();
                  break;
+                
+                case 404: 
+                 this.handle404();
+                 break;
+                 
+
+                default:
+                 this.handleDefaultError(errorObj);
              }
 
              return Observable.throw(errorObj);
         }) as any;
     }
 
+    handle401() {
+        let alert = this.alertCtrl.create({
+           title: 'Erro 401: falha de autenticação',
+           message: 'Email ou senha incorretos',
+           enableBackdropDismiss: false,
+           buttons: [
+               {
+                   text: 'Ok'
+               }            
+           ]
+        });
+
+        alert.present();
+    }
+
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handle404() {
+        let alert = this.alertCtrl.create({
+           title: 'Erro 404: Tela não encontrada',
+           message: 'Informações não encontradas para visualização',
+           enableBackdropDismiss: false,
+           buttons: [
+               {
+                   text: 'Ok'
+               }            
+           ]
+        });
+
+        alert.present();
+    }
+
+    handleDefaultError(errorObj) {
+        let alert = this.alertCtrl.create({
+           title: 'Erro' + errorObj.status + ': ' + errorObj.error,
+           message: errorObj.message,
+           enableBackdropDismiss: false,
+           buttons: [
+               {
+                   text: 'Ok'
+               }            
+           ]
+        });
+
+        alert.present();
     }
 }
 
