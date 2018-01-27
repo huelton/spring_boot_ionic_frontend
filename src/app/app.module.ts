eb10d3c2f8e692a1e92ff0d39596b11ba347f534
huelton.santos <hueltondihon@gmail.com>
2018-01-27 21:27:02 -0200

    Criando página de carrinho de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CartService } from './../services/domain/cart.service';
 import { AuthInterceptorProvider } from './../Interceptors/auth-interceptor';
 import { CLienteService } from './../services/domain/cliente.service';
 import { StorageService } from './../services/storage.service';
@@ -38,7 +39,8 @@ import { ProdutoService } from "../services/domain/produto.service";
     AuthService,
     StorageService,
     CLienteService,
-    ProdutoService   
+    ProdutoService,
+    CartService   
   ]
 })
 export class AppModule {}
--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,3 +1,4 @@
+import { CartService } from './../../services/domain/cart.service';
 import { API_CONFIG } from './../../config/api.config';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { ProdutoDTO } from './../../models/produto.dto';
@@ -17,7 +18,8 @@ export class ProdutoDetailPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -37,5 +39,10 @@ export class ProdutoDetailPage {
       },
      error => {});
 
+  }
+
+  addToCart(produto: ProdutoDTO){
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 }
